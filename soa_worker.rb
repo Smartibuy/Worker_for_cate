49af2b8d3c4c4dba8ba15bacd63472e330c7ac4c
Katy Wu <katy12032001@gmail.com>
2016-01-06 15:43:06 +0800

    worker for cate

---
+++ soa_worker.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+
+require_relative 'bundle/bundler/setup'
+require_relative 'config/config_queue.rb'
+require 'json'
+require 'aws-sdk'
+require 'httparty'
+# require 'jieba'
+require 'uri'
+
+puts "Starting SOA_worker at #{Time.now}"
+
+puts "Setting up AWS connection"
+config = JSON.parse(File.read('config/config.json'))
+ENV.update config
+sqs = Aws::SQS::Client.new
+q_url = QUEUE_URL
+
+puts "Polling SQS for messages"
+poller = Aws::SQS::QueuePoller.new(q_url)
+begin
+  collect = {}
+  poller.poll(wait_time_seconds:nil, idle_timeout:5) do |msg|
+
+    key = msg.body.to_s
+    # keyword_arr = HTTParty.post('http://workerapi.herokuapp.com/parse_keyword',:body => {"keyword" => keyword}.to_json, :headers => {'Content-Type' => 'application/json'})
+    # keyword_arr.each do |key|
+    if collect[key] != nil
+      collect[key] = collect[key] + 1
+    else
+      collect[key] = 1
+    end
+    # end
+  end
+
+  HTTParty.post('http://smartibuyapidynamo.herokuapp.com/api/v1/save_hot_cate',:body => {"cate_data" => collect}.to_json, :headers => {'Content-Type' => 'application/json'})
+
+  puts collect
+rescue Aws::SQS::Errors::ServiceError => e
+  puts "ERROR FROM SQS: #{e}"
+end
+
+puts "SOA_worker completed at #{Time.now}"
